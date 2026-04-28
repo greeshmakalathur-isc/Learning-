78885d8c0432d40794ef1bcd1d40a8a2cc0b0987
Greeshma <greeshmakalathur@gmail.com>
2026-04-28 12:27:49 +0000

    My Python learning files

---
+++ day2_3.py
@@ -0,0 +1,12 @@
+for index in range(1,21):
+  print(index)
+for index in range(1,21):
+  if index%2!=0:
+    print(index)
+
+
+for i in range(1, 21):
+    if i % 3 == 0:
+        continue
+    print(i)
+
