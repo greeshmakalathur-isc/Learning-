d3fd3822cda9c499a652a3226559288edd78b0a9
Greeshma <greeshmakalathur@gmail.com>
2026-04-30 04:18:40 +0000

    message

---
+++ day3_3.py
@@ -0,0 +1,7 @@
+series=["Dark","Stranger things","All of us are dead"]
+movies=["Anabelle","Conjuring","Secret"]
+combine=series+movies
+print(combine)
+print("shakthi" in combine)
+combine.reverse()
+print(combine[::2])
